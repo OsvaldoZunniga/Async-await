f9401f99a9c4934bdc017673898840e155dc918d
Katherine Solis <ktssolis36@gmail.com>
2026-06-10 20:40:00 -0600

    Rutas normales: parte 1
    Solo post

---
+++ pages/posts/[id].vue
@@ -0,0 +1,20 @@
+<script setup>
+const route = useRoute()
+const { data: post, loading, error, reload } = useApi(`/posts/${route.params.id}`)
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/posts">← Volver</NuxtLink>
+    <button @click="reload">Recargar</button>
+
+    <div v-if="loading">Cargando...</div>
+    <div v-else-if="error">Error: {{ error }}</div>
+    <div v-else-if="!post">Sin datos</div>
+    <div v-else>
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+      <p>User ID: {{ post.userId }}</p>
+    </div>
+  </div>
+</template>
